ed81f3293787de4c4165af68c3641701cfae5647
Xosrov <seyed.alireza.miryazdi@gmail.com>
2023-04-06 14:49:24 +0330

    Initial commit for bidirectional RabbitMQ transfers This will act as a communication channel between the streaming and hosting microservices. Connection configs have already been implemented in the config package
    Additionaly, errors for this new package are implemented in pkgerrors

---
+++ internal/rabbitmq/consumer.go
@@ -0,0 +1,38 @@
+package rabbitmq
+
+import (
+	"github.com/benu-cloud/benu-livestreaming-gst/pkg/pkgerrors"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func (r *RabbitMQConnection) NewConsumer(queuename string) (<-chan amqp.Delivery, error) {
+	ch, err := r.conn.Channel()
+	if err != nil {
+		return nil, pkgerrors.NewConsumeError(err)
+	}
+	defer ch.Close()
+	q, err := ch.QueueDeclare(
+		queuename,
+		false, // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // args
+	)
+	if err != nil {
+		return nil, pkgerrors.NewConsumeError(err)
+	}
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	if err != nil {
+		return nil, pkgerrors.NewConsumeError(err)
+	}
+	return msgs, nil
+}
---
+++ internal/rabbitmq/publisher.go
@@ -0,0 +1,51 @@
+package rabbitmq
+
+import (
+	"context"
+
+	"github.com/benu-cloud/benu-livestreaming-gst/internal/message"
+	"github.com/benu-cloud/benu-livestreaming-gst/pkg/pkgerrors"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func (r *RabbitMQConnection) Publish(queuename string, payload message.GenericPayload) error {
+	ch, err := r.conn.Channel()
+	if err != nil {
+		return pkgerrors.NewPublishError(err)
+	}
+	defer ch.Close()
+	q, err := ch.QueueDeclare(
+		queuename,
+		false, // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // args
+	)
+	if err != nil {
+		return pkgerrors.NewPublishError(err)
+	}
+
+	body, err := message.Marshal(payload)
+	if err != nil {
+		return pkgerrors.NewPublishError(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), r.publishTimeout)
+	defer cancel()
+
+	err = ch.PublishWithContext(ctx,
+		"",     // default exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        body,
+		},
+	)
+	if err != nil {
+		return pkgerrors.NewPublishError(err)
+	}
+	return nil
+}
